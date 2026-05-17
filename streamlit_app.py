6edeb8e60f55a0fd6d37cf63afecef4b5b25852b
RosAI-27 <siapzerose27@gmail.com>
2026-05-17 06:13:03 +0100

    Remove unused math import
    Removed unused import of the math module.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import math
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
